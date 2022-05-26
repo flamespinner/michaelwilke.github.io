97ae1849d62c6db13822b28688c603260bcd5bb6
Michael Wilke <repair@wilkerepair.com>
2022-05-26 02:07:36 -0500

    Labeled Projects.

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -4,113 +4,113 @@ export default class Projects extends Component {
   render() {
     return (
       <div>
-				<section className="colorlib-work" data-section="projects">
-					<div className="colorlib-narrow-content">
-						<div className="row">
-							<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-								<span className="heading-meta">My Work</span>
-								<h2 className="colorlib-heading animate-box">Recent Projects</h2>
-							</div>
-						</div>
-						<div className="row">
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
-								<div className="project" style={{backgroundImage: 'url(images/img-1.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">CampFire Bot</a></h3>
-											<span>Twitch/Discord Bot</span>
-											<p className="icon">
-												{/*<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>*/}
-											</p>
-										</div>
-									</div>
+		<section className="colorlib-work" data-section="projects">
+			<div className="colorlib-narrow-content">
+				<div className="row">
+					<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+						<span className="heading-meta">My Work</span>
+						<h2 className="colorlib-heading animate-box">Recent Projects</h2>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+						<div className="project" style={{backgroundImage: 'url(images/img-1.jpg)'}}>
+							<div className="desc">
+								<div className="con">
+									<h3><a href="campfirebot.html">CampFire Bot</a></h3>
+									<span>Twitch/Discord Bot</span>
+									<p className="icon">
+										{/*<span><a href="#"><i className="icon-share3" /></a></span>
+										<span><a href="#"><i className="icon-eye" /> 100</a></span>
+										<span><a href="#"><i className="icon-heart" /> 49</a></span>*/}
+									</p>
 								</div>
-							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
-								<div className="project" style={{backgroundImage: 'url(images/img-2.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">BB-8</a></h3>
-											<span>Robot</span>
-											<p className="icon">
-												{/*<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>*/}
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInTop">
-								<div className="project" style={{backgroundImage: 'url(images/img-3.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">R2D2</a></h3>
-											<span>Robot</span>
-											<p className="icon">
-												{/*<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>*/}
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							{/*<div className="col-md-4 animate-box" data-animate-effect="fadeInBottom">
-								<div className="project" style={{backgroundImage: 'url(images/img-4.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Work 04</a></h3>
-											<span>Application</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
-								<div className="project" style={{backgroundImage: 'url(images/img-5.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Work 05</a></h3>
-											<span>Graphic, Logo</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
-								<div className="project" style={{backgroundImage: 'url(images/img-6.jpg)'}}>
-									<div className="desc">
-										<div className="con">
-											<h3><a href="work.html">Work 06</a></h3>
-											<span>Web Design</span>
-											<p className="icon">
-												<span><a href="#"><i className="icon-share3" /></a></span>
-												<span><a href="#"><i className="icon-eye" /> 100</a></span>
-												<span><a href="#"><i className="icon-heart" /> 49</a></span>
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>*/}
-						</div>
-						<div className="row">
-							<div className="col-md-12 animate-box">
-								<p><a href="#" className="btn btn-primary btn-lg btn-load-more">Load more <i className="icon-reload" /></a></p>
 							</div>
 						</div>
 					</div>
-				</section>
+					<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
+						<div className="project" style={{backgroundImage: 'url(images/img-2.jpg)'}}>
+							<div className="desc">
+								<div className="con">
+									<h3><a href="homelab.html">HomeLab</a></h3>
+									<span>Robot</span>
+									<p className="icon">
+										{/*<span><a href="#"><i className="icon-share3" /></a></span>
+										<span><a href="#"><i className="icon-eye" /> 100</a></span>
+										<span><a href="#"><i className="icon-heart" /> 49</a></span>*/}
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-4 animate-box" data-animate-effect="fadeInTop">
+						<div className="project" style={{backgroundImage: 'url(images/img-3.jpg)'}}>
+							<div className="desc">
+								<div className="con">
+									<h3><a href="starwars.html">Star Wars Robots</a></h3>
+									<span>Robot</span>
+									<p className="icon">
+										{/*<span><a href="#"><i className="icon-share3" /></a></span>
+										<span><a href="#"><i className="icon-eye" /> 100</a></span>
+										<span><a href="#"><i className="icon-heart" /> 49</a></span>*/}
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-4 animate-box" data-animate-effect="fadeInBottom">
+						<div className="project" style={{backgroundImage: 'url(images/img-4.jpg)'}}>
+							<div className="desc">
+								<div className="con">
+									<h3><a href="first.html"><i>FIRST</i> Robotics</a></h3>
+									<span>TEXT</span>
+									<p className="icon">
+										{/*<span><a href="#"><i className="icon-share3" /></a></span>
+										<span><a href="#"><i className="icon-eye" /> 100</a></span>
+									<span><a href="#"><i className="icon-heart" /> 49</a></span>*/}
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					{/*<div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+						<div className="project" style={{backgroundImage: 'url(images/img-5.jpg)'}}>
+							<div className="desc">
+								<div className="con">
+									<h3><a href="work.html">Work 05</a></h3>
+									<span>Graphic, Logo</span>
+									<p className="icon">
+										<span><a href="#"><i className="icon-share3" /></a></span>
+										<span><a href="#"><i className="icon-eye" /> 100</a></span>
+										<span><a href="#"><i className="icon-heart" /> 49</a></span>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
+						<div className="project" style={{backgroundImage: 'url(images/img-6.jpg)'}}>
+							<div className="desc">
+								<div className="con">
+									<h3><a href="work.html">Work 06</a></h3>
+									<span>Web Design</span>
+									<p className="icon">
+										<span><a href="#"><i className="icon-share3" /></a></span>
+										<span><a href="#"><i className="icon-eye" /> 100</a></span>
+										<span><a href="#"><i className="icon-heart" /> 49</a></span>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>*/}
+				</div>
+				<div className="row">
+					<div className="col-md-12 animate-box">
+						<p><a href="#" className="btn btn-primary btn-lg btn-load-more">Load more <i className="icon-reload" /></a></p>
+					</div>
+				</div>
+			</div>
+		</section>
       </div>
     )
   }
